c6970f9d854bf16bf04d41b476e2f058518d7443
banelengwane <banelengwane@gmail.com>
2024-01-03 10:50:46 +0200

    rendering list items

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "Charger", quantity: 1, packed: true },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -20,12 +26,31 @@ function Form() {
   );
 }
 function PackingList() {
-  return <div className="list">List</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
 }
 function Stats() {
   return (
-    <footer classname="stats">
+    <footer className="stats">
       <em>💼You have X itms on your list, and you already packed X (X%)</em>
     </footer>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
   );
 }
